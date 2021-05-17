8f847db1afd1bd0db6f212a9c5c72d8dfd194fe2
toshiki38 <t2738d@gmail.com>
2021-05-17 23:54:51 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return (
-    <div className="App">
-	  This is a template for React app.
+	const init = 0;
+  const [count,setCount] = useState(init);
 
-    </div>
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
+		
+	const increment2 = () => setCount((previousCount) => {return previousCount+1;});
+	const decrement2 = () => setCount(previousCount => previousCount - 1);
+
+	const reset = () => setCount(init);
+	const double = () => setCount(previousCount => previousCount * 2);
+	const divideBy3 = () => setCount(previousCount => {
+		if (previousCount % 3 === 0){
+			return previousCount / 3;
+		} else {
+			return previousCount;
+		}});
+
+  return (
+		<>
+			<div>count: {count}</div>
+			<div>
+				<button onClick={increment}>+1</button>
+				<button onClick={decrement}>-1</button>
+			</div>
+			<div>
+				<button onClick={increment2}>+1</button>
+				<button onClick={decrement2}>-1</button>
+			</div>
+			<div>
+				<button onClick={reset}>Reset</button>
+				<button onClick={double}>×2</button>
+				<button onClick={divideBy3}>3の倍数の時だけ3で割る</button>
+			</div>
+		</>
   );
 }
 
