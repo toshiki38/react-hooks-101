49e2e607c6dd2518c7cf190e60895c3a54c00363
toshiki38 <t2738d@gmail.com>
2021-05-22 22:04:27 +0900

    create event form and event table

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <div className="App">
-	  This is a template for React app.
-
+    <div className="conatiner-fluid">
+			<h4>イベント作成フォーム</h4>
+			<form>
+				<div className="form-group">
+					<label htmlFor="formEventTitle">タイトル</label>
+					<input className="form-control" id="formEventTitle"/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="formEventBody">ボディ</label>
+					<textarea className="form-control" id="formEventBody"/>
+				</div>
+				<button className="btn btn-primary">イベントを作成する</button>
+				<button className="btn btn-danger">全てのイベントを削除する</button>
+			</form>
+			<h4>イベント一覧</h4>
+			<table className="table table-hover">
+				<thead>
+					<th>ID</th>
+					<th>タイトル</th>
+					<th>ボディ</th>
+					<th></th>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
     </div>
   );
 }
